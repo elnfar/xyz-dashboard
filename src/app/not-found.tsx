b09a8de307476cee25d980b0b7a239ae4f05370a
elnfar <farzaliyevelnur00@gmail.com>
2024-09-23 19:15:22 +0300

    fix: ' sign fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className='w-full items-center flex h-screen justify-center flex-col bg-black text-white'>
-      <h1 className=' font-bold text-3xl'>Oops, it looks like this page doesn't exist, please go back.</h1>
+      <h1 className=' font-bold text-3xl'>Oops, it looks like this page do not exist, please go back.</h1>
       <Link className='text-blue-400' href='/'>Home page</Link>
     </div>
   )
